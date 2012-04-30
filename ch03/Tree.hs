dd3ba02069c1f701847875bd42812b2bcaf26da4
Vincent Jousse <vincent@jousse.org>
2012-04-30 17:55:44 +0200

    Count tree size using recursion

--- ch03/Tree.hs
+++ ch03/Tree.hs
@@ -3,7 +3,16 @@ data Tree a = Node a (Tree a) (Tree a)
             deriving (Show)
 
 simpleTree = Node "parent" (Node "Left child" Empty Empty)
-                           (Node "Right child" Empty Empty)
+                           (Node "Right child" (Node "Level 3" Empty (Node "Level 4" Empty Empty)) Empty)
+
+
+-- Count Tree tree size using recursion
+treeSize :: Tree[a] -> Int
+treeSize Empty                = 0
+treeSize (Node a Empty Empty) = 1
+treeSize (Node a Empty r)     = 1 + treeSize r
+treeSize (Node a l Empty)     = 1 + treeSize l
+treeSize (Node a l r)         = max (1 + treeSize l) (1 + treeSize r)
 
 
 data MaybeTree a = MaybeNode a (Maybe (MaybeTree a)) (Maybe (MaybeTree a))
